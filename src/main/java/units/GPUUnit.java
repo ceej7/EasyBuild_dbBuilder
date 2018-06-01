29c51fcf85fc7d7060fe0b9ede15fc12ee454942
ceej7 <ceej_7@hotmail.com>
2018-06-01 10:46:32 +0800

    GPU unit added

---
+++ src/main/java/units/GPUUnit.java
@@ -0,0 +1,19 @@
+package units;
+
+public class GPUUnit {
+    String Name;
+    String Chip;
+    String Released;
+    String Bus;
+    int Memory_Size;
+    String Memory_Type;
+    int Memory_Bus;
+    int GPU_Clock;
+    int M_Clock;
+    int Shaders;
+    int TMUs;
+    int ROPs;
+    int Multiplier;
+
+
+}
