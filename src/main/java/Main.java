136dcdd29c3a5cefd042d663015460a15802a116
ceej7 <ceej_7@hotmail.com>
2018-06-01 14:09:17 +0800

    GPU part is finished

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import crawl.CrawlerCPUData;
 import crawl.CrawlerDemo;
+import crawl.CrawlerGPUData;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,7 +10,7 @@ public class Main {
 //        new CrawlerDemo("https://list.jd.com/list.html?cat=670,677,683&page={page}","F:\\HDDlog.txt").StartCrawling();
 //        new CrawlerDemo("https://list.jd.com/list.html?cat=670,677,11303&page={page}","F:\\SSDlog.txt").StartCrawling();
 //        new CrawlerDemo("https://list.jd.com/list.html?cat=670,677,680&page={page}","F:\\Memorylog.txt").StartCrawling();
-        new CrawlerCPUData("C:\\Users\\ceej_\\Desktop\\CPUDATA.txt").Start();
-
+        //new CrawlerCPUData("C:\\Users\\ceej_\\Desktop\\CPUDATA.txt").Start();
+            new CrawlerGPUData("C:\\Users\\ceej_\\Desktop\\GPUDATA.txt").Start();
     }
 }
--- src/main/java/units/GPUUnit.java
+++ src/main/java/units/GPUUnit.java
@@ -43,8 +43,30 @@ public class GPUUnit {
         if(str.length==4)
         {
             Shaders=Integer.parseInt( str[0].split("\\D+")[0]   )+Integer.parseInt( str[1].split("\\D+")[0]   );
-
+            TMUs=Integer.parseInt( str[2].split("\\D+")[0]   );
+            ROPs=Integer.parseInt( str[3].split("\\D+")[0]   );
+        }
+        else{
+            Shaders=Integer.parseInt( str[0].split("\\D+")[0]   );
+            TMUs=Integer.parseInt( str[1].split("\\D+")[0]   );
+            ROPs=Integer.parseInt( str[2].split("\\D+")[0]   );
         }
     }
 
+    @Override
+    public String toString() {
+        return  Name+" "+
+         Chip+" "+
+         Released+" "+
+         Bus+" "+
+         Memory_Size+" "+
+         Memory_Type+" "+
+         Memory_Bus+" "+
+         GPU_Clock+" "+
+         M_Clock+" "+
+         Shaders+" "+
+         TMUs+" "+
+         ROPs+" "+
+         Multiplier;
+    }
 }
