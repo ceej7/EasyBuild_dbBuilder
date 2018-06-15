5198e90ca587f4d68b848a5679e53e57edcd6816
ceej7 <ceej_7@hotmail.com>
2018-06-15 14:38:09 +0800

    Data is partly transitted to local MongoDB`

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,11 +1,13 @@
 package main;
 
+import DailyMaintance.ItemsUpdate;
 import crawl.CrawlerCPUData;
 import crawl.CrawlerDemo;
 import crawl.CrawlerGPUData;
 
 public class Main {
     public static void main(String[] args) {
-
+        //更新item价格和comments的核心线程，每日调用即可
+        new ItemsUpdate().StartCrawling();
     }
 }
